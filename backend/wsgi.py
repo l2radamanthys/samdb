9efc53389590277ad34d37fb5e0e89e723df0aba
Ricardo D. Quiroga <l2radamanthys@gmail.com>
2023-10-05 21:45:53 -0300

    wsgi

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -4,6 +4,8 @@ from django.core.wsgi import get_wsgi_application
 from whitenoise import WhiteNoise
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
+
+BASE_DIR = Path(__file__).resolve().parent.parent
 WHITE_NOISE_ENABLED = os.environ.get('WHITE_NOISE_ENABLED', None) == 'on'
 
 application = get_wsgi_application()
